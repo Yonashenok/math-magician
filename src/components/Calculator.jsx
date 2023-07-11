594259b6e7ea8110b299b0c6127a251b0e798ff3
Yonashenok <yonashenok24@gmail.com>
2023-07-11 14:37:05 +0300

    Feat: quote is display on with calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,6 +4,29 @@ import classes from './Calculator.module.css';
 import KeyPad from './KeyPad';
 
 const Calculator = () => {
+  // const apiKey = 'I/eJb/FRt3dKtYB18V0/LA==Bh0PtvNyvoOR3M47';
+
+  // useEffect(() => {
+  //   const Quotes = async () => {
+  //     try {
+  //       // setData((prevState) => ({ ...prevState, isLoading: true }));
+  //       const url = 'https://api.api-ninjas.com/v1/facts?limit=4';
+  //       const res = await fetch(url, {
+  //         method: 'GET',
+  //         headers: { 'X-Api-Key': apiKey },
+  //         contentType: 'application/json',
+  //       });
+  //       const result = await res.json();
+  //       console.log(result);
+  //       // setData((prevState) => ({ ...prevState, isData: result }));
+  //     } catch (err) {
+  //       // setData((prevState) => ({ ...prevState, isError: true }));
+  //       console.log(err);
+  //     }
+  //   };
+  //   Quotes();
+  // });
+
   const [result, setResult] = useState({
     total: null,
     next: null,
